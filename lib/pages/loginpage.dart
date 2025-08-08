7a08061ca6a31725d1762ed66f4daf5e6bcbb52e
Sushant Neupane <nsushaant72@gmail.com>
2025-08-08 20:18:55 +0545

    login screen started

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -4,7 +4,41 @@ class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
+
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Theme.of(context).colorScheme.surface);
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //!logo
+            Icon(
+              Icons.message,
+              size: 50,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+
+            //!welcome back message
+            SizedBox(height: 20),
+            Text('Welcome back buddy kata harayau ',
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+              fontSize: 16
+            ),),
+
+            //email text field
+             SizedBox(height: 20),
+             TextField()
+
+            //pw textfield
+
+            //login button
+
+            //register now
+          ],
+        ),
+      ),
+    );
   }
 }
