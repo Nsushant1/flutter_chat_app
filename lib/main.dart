4e8e1e956a1b7b383749b8e69aaf16925dfaa3da
Sushant Neupane <nsushaant72@gmail.com>
2025-09-25 19:57:15 +0545

    worked on chat app

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,38 @@
+import 'package:chat_app/firebase_options.dart';
+import 'package:chat_app/provider/auth_provider.dart';
+import 'package:chat_app/provider/chat_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApp());
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  class MyApp extends StatelessWidget {
+    const MyApp({super.key});
+  
+    @override
+    Widget build(BuildContext context) {
+      return MultiProvider(providers: [
+        ChangeNotifierProvider(create: (_)=> AuthProvider()),
+        ChangeNotifierProvider(create: (_)=> ChatProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: AuthenticationWrapper(),
+      ),);
+    }
+  }
+  class AuthenticationWrapper extends StatelessWidget {
+  const AuthenticationWrapper({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Consumer<AuthProvider>();
   }
 }
+
+
